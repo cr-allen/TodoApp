f3b955aaca2da9bc25a0e187bdb0a6b8e190ffff
Augustine LF <c.r.allen310@gmail.com>
2017-12-11 14:54:16 -0800

    attempted to make it prettier, fiddled with edit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,62 @@
 // research: functions, calling functions, uuuh, dissect the codepen better. also css grids //
 
 //var query selectors
-var taskInput=document.querySelector("toDoItem");
-var addButton=document.querySelector("addButton");
-var toDo=document.querySelector("toDo");
-var done=document.querySelector("done");
+var taskInput=document.querySelector("#toDoItem");
+var addButton=document.querySelector("#addButton");
+var toDo=document.querySelector("#toDo");
+var done=document.querySelector("#done");
+var date = document.querySelector("#date");
 
+console.log(addButton);
 // functions and constructors
 
 ///functions
-var createNewItem = function taskString() {
+/// create new task
+var createNewItem = function(taskString) {
+  // need to bind this to the form
+  var userInput = document.querySelector('#toDoItem');
   var todoItem = document.createElement("li");
-
   var label = document.createElement("label");
   var checkboxInput = document.createElement("input") ;
-  var editInput = document.createElement("input");
-  var editButton = document.createElement("button");
+  // var editInput = document.createElement("input");
+  var editButton = document.createElement("button"); //not showing up WHY
   var deleteButton = document.createElement("button");
+  var dueDate = document.createElement('p')
+
 
   label.innerText = taskString;
+
   //specifics for elements
-  input.type="checkbox";
-  editInput.type="text";
+  checkboxInput.type="checkbox";
+  // editInput.type="text";
   editButton.className="edit"
-  deleteButton.innerText="Delete";
-  deleteButton.className="delete";
+  deleteButton.innerText="delete";
+  deleteButton.className="deleteItem";
+
 
   //appends
   todoItem.appendChild(label);
   todoItem.appendChild(checkboxInput);
-  todoItem.appendChild(editInput);
+  // todoItem.appendChild(editInput);
   todoItem.appendChild(editButton);
   todoItem.appendChild(deleteButton);
+  toDoItem.setAttribute("class","editMode");
   return todoItem;
+    console.log("stuff");
 };
-////^^^^ONCLICK ISN'T WORKING WTF RESEARCH IT
+////^^^^set attribute for editMode and trigger edit onclick of button
+
+
 
 ///add task
-var addTask = function functionName() {
+var addTask = function() {
+  console.log('adding tasks');
+  var li = document.querySelector
   var listItem = createNewItem(taskInput.value);
+  var dueDate = createNewItem(date.value)
   toDo.appendChild(listItem);
-  bindTaskEvents(listItem,tasksDone)
+  bindTaskEvents(listItem,finishedItems);
+
 
   //specifics
   var editable = document.querySelector("li");
@@ -49,25 +65,25 @@ var addTask = function functionName() {
 
   taskInput="";
 };
+//// only works the first tie used, wtf
 
 ///bindTaskEvents
-var bindTaskEvents=function(listItem,checkboxEvent) {
+var bindTaskEvents=function(todoItem,checkboxEvent) {
   console.log("bind events");
   //select children
-  var checkbox = listItem.querySelector("input[type="checkbox"]");
-  var editButton = listItem.querySelector("button.edit");
-  var deleteButton = listItem.querySelector("button.delete");
+  var checkbox = todoItem.querySelector('input[type="checkbox"]');
+  var editButton = todoItem.querySelector("button.edit");
+  var deleteButton = todoItem.querySelector("button.deleteItem");
 
   editButton.onclick=editTask;
   deleteButton.onclick=deleteTask;
-  checkbox.ohchange=checkboxEvent; //where is this defined???
-}
+  checkbox.onchange=checkboxEvent; //where is this defined???
+};
 
-///edit task
+///edit task <--- does NOTHING
 var editTask=function() {
-  var taskInput=this.parentNode; //binds to checkbox
-  var label = querySelector("label"); //grabs label
-  var containsClass=listItem.classList.contains("editMode"); //element might need to be li??
+  var label = document.querySelector("label"); //grabs label
+  var containsClass=todoItem.classList.contains("editMode"); //element might need to be li??
   if (containsClass) {
     label.innerText=editInput.value;
   } else {
@@ -75,37 +91,27 @@ var editTask=function() {
   }
   listItem.classList.toggle("editMode");
 };
-//// ^^^ will call on taskInput, turn it back into an input. add submit button??
+// ^^^ will call on taskInput, turn it back into an input. add submit button??
 
 
 ///delete task
-var deleteTask = function () {
-  var listItem=this.parentNode; // binds to ul?
-  var ul=listItem.parentNode; //not sure
-  ul.removeChild(listItem);
+var deleteTask = function() {
+  var ul= document.querySelector('ul');
+  var li= document.querySelector('li');
+  ul.removeChild('li');
 };
 
 
-///date task - date picker that will sit between input and button.
-
-
-///deadline countdown - will go next to the item in the li.
-
-
-///drag and drop organizing
-
-
-///task done
-var finishedItems = function () {
+///task done = works!!!
+var finishedItems = function() {
+  console.log(this);
   var listItem=this.parentNode; //binds to checkbox again
   done.appendChild(listItem);
-    bindTaskEvents(listItem, taskIncomplete)
+    bindTaskEvents(listItem);
 };
 
 
-///task taskIncomplete - necessary so can be used in bindTaskEvents arguments
+//event listeners - need function??
+addButton.addEventListener("click", addTask);
 
-
-
-
-//event listeners
+//
